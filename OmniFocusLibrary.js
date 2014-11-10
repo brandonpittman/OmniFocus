da30b6fd7ce692fa1ef94bf445f6cfff787c31c4
Brandon Pittman <brandonpittman@gmail.com>
2014-11-10 19:07:55 +0900

    Add semicolons
    Semicolons make JSHint shut the fuck up.

--- OmniFocusLibrary.js
+++ OmniFocusLibrary.js
@@ -1,125 +1,116 @@
-app = Application('OmniFocus')
-app.includeStandardAdditions = true
-doc = app.defaultDocument
+app = Application('OmniFocus');
+app.includeStandardAdditions = true;
+doc = app.defaultDocument;
 
 function selected() {
-	return app.windows[0].content.selectedTrees.value()
+	return app.windows[0].content.selectedTrees.value();
 }
 
 function searchString(term) {
-	return new RegExp(term, 'i')
+	return new RegExp(term, 'i');
 }
 
 function allTasks() {
-	return doc.flattenedTasks.whose({completed: false})()
+	return doc.flattenedTasks.whose({completed: false})();
 }
 
 function allProjects() {
-	return doc.flattenedProjects.whose({completed: false})()
+	return doc.flattenedProjects.whose({completed: false})();
 }
 
 function tasksWithContext(context) {
-	tasks = []
+	tasks = [];
 	allTasks().forEach(function(task, index) {
-		if (task.context() != null) {
+		if (task.context() !== null) {
 			if (searchString(context).test(task.context().name())) {
-				tasks.push(task)
-				console.log(task.name())
-				console.log(tasks.length)
+				tasks.push(task);
 			}
 		}
-	})
-	return tasks
+	});
+	return tasks;
 }
 
 function projectsWithName(name) {
-	projects = []
+	projects = [];
 	allProjects().forEach(function(project, index) {
-		if (project.container() != null) {
+		if (project.container() !== null) {
 			if (searchString(name).test(project.name())) {
-				projects.push(project)
-				console.log(project.name())
-				console.log(projects.length)
+				projects.push(project);
 			}
 		}
-	})
-	return projects
+	});
+	return projects;
 }
 
 function tasksWithName(name) {
-	tasks = []
+	tasks = [];
 	allTasks().forEach(function(task, index) {
 		if (searchString(name).test(task.name())) {
-			tasks.push(task)
-			console.log(task.name())
-			console.log(tasks.length)
+			tasks.push(task);
 		}
-	})
-	return tasks
+	});
+	return tasks;
 }
 
 function allWithName(searchTerm) {
-	tasks = tasksWithName(searchTerm).concat(tasksWithContext(searchTerm)).concat(projectsWithName(searchTerm))
-	return tasks
+	tasks = tasksWithName(searchTerm).concat(tasksWithContext(searchTerm)).concat(projectsWithName(searchTerm));
+	return tasks;
 }
 
 function setDefer(tasks, date) {
 	tasks.forEach(function (task) {
 		date.getHours() === 0 ? date.setHours(8) : date
-		console.log(date)
-		task.deferDate = date
-	})
+		task.deferDate = date;
+	});
 }
 
 function setDue(tasks, date) {
 	tasks.forEach(function (task) {
 		date.getHours() === 0 ? date.setHours(4) : date
-		console.log(date)
-		task.dueDate = date
-	})
+		task.dueDate = date;
+	});
 }
 
 function setContext(tasks, place) {
-	newContext = doc.flattenedContexts.whose({name: place})[0]
+	newContext = doc.flattenedContexts.whose({name: place})[0];
 	tasks.forEach(function(task) {
-		task.context = newContext
-		console.log(task.name() + "'s context is now set to " + newContext.name() + '.')
-	})
+		task.context = newContext;
+	});
 }
 
 function getContext(context) {
-	return doc.flattenedContexts.whose({name: context})[0]
+	return doc.flattenedContexts.whose({name: context})[0];
 }
 
 function getProject(project) {
-	return doc.flattenedProjects.whose({name: project})[0]
+	return doc.flattenedProjects.whose({name: project})[0];
 }
 
 function inboxTasks() {
-	return doc.inboxTasks()
+	return doc.inboxTasks();
 }
 
 function newTask(text, place, deferDate, dueDate) {
-	return app.Task({name: text, context: getContext(place), deferDate: null || deferDate, dueDate: null || dueDate})
+	return app.Task({name: text, context: getContext(place), deferDate: null || deferDate, dueDate: null || dueDate});
 }
 
 function pushTask(task, project) {
-	getProject(project).tasks.push(task)
+	getProject(project).tasks.push(task);
 }
 
 function updateInboxTasks(context, project, deferDate, dueDate) {
-	newContext = getContext(context)
-	newProject = getProject(project)
+	newContext = getContext(context);
+	newProject = getProject(project);
 	inboxTasks().forEach(function(task) {
-		task.context = newContext
-		task.deferDate = deferDate
-		task.dueDate = dueDate
-		pushTask(task, newProject)
-	})
+		task.context = newContext;
+		task.deferDate = deferDate;
+		task.dueDate = dueDate;
+		pushTask(task, newProject);
+	});
 }
 
 function prefixTasksWith(tasks,text) {
 	tasks.forEach(function(task) {
-		task.name = text + ' ' + task.name()
-	})
+		task.name = text + ' ' + task.name();
+	});
 }
