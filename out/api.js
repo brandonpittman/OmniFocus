3ff9e34c92957041f98d724ffb4ffa2b9a71ec07
brandonpittman <brandonpittman@gmail.com>
2014-11-10 21:30:59 +0900

    Add docs

---
+++ out/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Getters",
+        "Properties",
+        "Setters"
+    ],
+    "modules": [
+        "OmniFocus"
+    ],
+    "allModules": [
+        {
+            "displayName": "OmniFocus",
+            "name": "OmniFocus"
+        }
+    ]
+} };
+});
