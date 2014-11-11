02a5105318dc41ccf69bb6bb4eaefa196b474d64
brandonpittman <brandonpittman@gmail.com>
2014-11-11 11:19:59 +0900

    Add .gitignore

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jsdoc : {
+        dist : {
+            src: ['./*.js', 'README.md'],
+            jsdoc: './node_modules/.bin/jsdoc',
+            options: {
+                destination: 'doc',
+                configure: './node_modules/ink-docstrap/template/jsdoc.conf.json',
+                template: './node_modules/ink-docstrap/template'
+            }
+        }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-jsdoc');
+
+};
