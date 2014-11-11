ba48d09a275fba3f674610884bbc663be5069c9d
brandonpittman <brandonpittman@gmail.com>
2014-11-11 11:18:34 +0900

    Update docs

--- OmniFocusLibrary.js
+++ OmniFocusLibrary.js
@@ -51,6 +51,12 @@ function allProjects() {
 	return doc.flattenedProjects.whose({completed: false})();
 }
 
+/**
+ * @method tasksWithContext
+ * @param {string} context Context name to search for
+ * @param {array} [inputTasks=allTasks()] Array of tasks to search through
+ * @return {array} Array of tasks that belong to contexts matching `context`
+ */
 function tasksWithContext(context, inputTasks) {
 	tasks = [];
 	tasksToProcess = inputTasks ? inputTasks : allTasks();
@@ -64,6 +70,14 @@ function tasksWithContext(context, inputTasks) {
 	return tasks;
 }
 
+/**
+*
+* @method projectsWithName
+* @param {string} [name] Name of projects to search
+* @param {array} [inputProjects=allProjects()] Array of projects to search
+* @return {array} Array of matching projects
+*
+*/
 function projectsWithName(name, inputProjects) {
 	projects = [];
 	projectsToProcess = inputProjects ? inputProjects : allProjects();
@@ -77,6 +91,14 @@ function projectsWithName(name, inputProjects) {
 	return projects;
 }
 
+/**
+*
+* @method tasksWithName
+* @param {string} [name] name of tasks to search for
+* @param {array} [inputTasks=allTasks()] tasks to search
+* @return {array} Matched tasks
+*
+*/
 function tasksWithName(name, inputTasks) {
 	tasks = [];
 	tasksToProcess = inputTasks ? inputTasks : allTasks();
@@ -88,11 +110,25 @@ function tasksWithName(name, inputTasks) {
 	return tasks;
 }
 
-function allWithName(searchTerm, inputTasks) {
+/**
+*
+* @method allWithName
+* @param {string} [searchTerm] Term to search for
+* @return {array} Matching tasks
+*
+*/
+function allWithName(searchTerm) {
 	tasks = tasksWithName(searchTerm).concat(tasksWithContext(searchTerm)).concat(projectsWithName(searchTerm));
 	return tasks;
 }
 
+/**
+*
+* @method setDefer
+* @param {array} [tasks] Tasks to defer
+* @param {object} [date] Date to defer tasks to
+*
+*/
 function setDefer(tasks, date) {
 	tasks.forEach(function (task) {
 		date.getHours() === 0 ? date.setHours(8) : date
@@ -100,6 +136,13 @@ function setDefer(tasks, date) {
 	});
 }
 
+/**
+*
+* @method setDue
+* @param {array} [tasks] Tasks to make due
+* @param {object} [date] Date to make tasks due on
+*
+*/
 function setDue(tasks, date) {
 	tasks.forEach(function (task) {
 		date.getHours() === 0 ? date.setHours(4) : date
@@ -107,33 +150,87 @@ function setDue(tasks, date) {
 	});
 }
 
-function setContext(tasks, place) {
-	newContext = doc.flattenedContexts.whose({name: place})[0];
+/**
+*
+* @method setContext
+* @param {array} [tasks] Tasks to set context for
+* @param {string} [context] Context to set tasks to
+*
+*/
+function setContext(tasks, context) {
+	newContext = doc.flattenedContexts.whose({name: context})[0];
 	tasks.forEach(function(task) {
 		task.context = newContext;
 	});
 }
 
+/**
+*
+* @method getContext
+* @param {string} [context] Context name to search for
+* @return {object} Context object
+*
+*/
 function getContext(context) {
 	return doc.flattenedContexts.whose({name: context})[0];
 }
 
+/**
+*
+* @method getProject
+* @param {string} [project] Project name to search for
+* @return {object} Project object
+*
+*/
 function getProject(project) {
 	return doc.flattenedProjects.whose({name: project})[0];
 }
 
+/**
+*
+* @method inboxTasks
+* @return {array} All inbox tasks
+*
+*/
 function inboxTasks() {
 	return doc.inboxTasks();
 }
 
-function newTask(text, place, deferDate, dueDate) {
-	return app.Task({name: text, context: getContext(place), deferDate: null || deferDate, dueDate: null || dueDate});
+/**
+*
+* @method newTask
+* @param {string} [name] Name of new task
+* @param {string} [context] Context of new task
+* @param {object} [deferDate] Defer date of new task
+* @param {object} [dueDate] Due date of new task
+* @return {object} New task object
+*
+*/
+function newTask(text, context, deferDate, dueDate) {
+	return app.Task({name: text, context: getContext(context), deferDate: null || deferDate, dueDate: null || dueDate});
 }
 
+/**
+*
+* @method pushTask
+* @param {object} [task] Task object to add to `project`
+* @param {string} [project] Name of project to add task to
+* @return {number} Number of tasks in project after adding new task
+*/
 function pushTask(task, project) {
 	getProject(project).tasks.push(task);
 }
 
+/**
+*
+* @method updateInboxTasks
+* @param {string} [context] Context of new task
+* @param {string} [project] Name of project for new task
+* @param {object} [deferDate] Defer date of new task
+* @param {object} [dueDate] Due date of new task
+* @return {object} New task object
+*
+*/
 function updateInboxTasks(context, project, deferDate, dueDate) {
 	newContext = getContext(context);
 	newProject = getProject(project);
@@ -145,6 +242,13 @@ function updateInboxTasks(context, project, deferDate, dueDate) {
 	});
 }
 
+/**
+*
+* @method prefixTasksWith
+* @param {array} [tasks] Tasks to modify
+* @param {string} [text] Text to prefix with
+*
+*/
 function prefixTasksWith(tasks,text) {
 	tasks.forEach(function(task) {
 		task.name = text + ' ' + task.name();
