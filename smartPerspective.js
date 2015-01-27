30dfa565cbd4f11a276e868d58078530238088ce
brandonpittman <brandonpittman@gmail.com>
2015-01-28 08:08:23 +0900

    Refactor smartPerspective.js

--- smartPerspective.js
+++ smartPerspective.js
@@ -1,26 +1,11 @@
 var of = Library('OmniFocus');
-var app = Application('OmniFocus');
-app.includeStandardAdditions = true;
-var doc = app.defaultDocument;
 
-function openPerspective(perName) {
-	app.launch();
-	var window = app.windows[0];
-	if (window.visible()) {
-		window.perspectiveName = perName;
-	}
-}
-
-var inboxCount = doc.inboxTasks.length;
-var firstCount = of.getContext('First Thing').availableTaskCount();
-var flaggedCount = doc.flattenedTasks.whose({completed: false, flagged: true, blocked: false}).length;
-
-if (inboxCount > 0) {
-	openPerspective("Inbox");
-}	else if (firstCount > 0) {
-	openPerspective('First Thing');
-} else if (flaggedCount > 0) {
-	openPerspective("Flagged");
+if (of.inboxCount > 0) {
+	of.openPerspective("Inbox");
+}	else if (of.firstCount > 0) {
+	of.openPerspective('First Thing');
+} else if (of.flaggedCount > 0) {
+	of.openPerspective("Flagged");
 } else {
-	openPerspective("Forecast");
+	of.openPerspective("Upcoming");
 }
