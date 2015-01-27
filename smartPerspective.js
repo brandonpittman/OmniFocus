98929a1c1f106f30c91987f8a28923132642f994
brandonpittman <brandonpittman@gmail.com>
2015-01-28 08:11:31 +0900

    Fix typo

--- smartPerspective.js
+++ smartPerspective.js
@@ -1,10 +1,10 @@
 var of = Library('OmniFocus');
 
-if (of.inboxCount > 0) {
+if (of.inboxCount() > 0) {
 	of.openPerspective("Inbox");
-}	else if (of.firstCount > 0) {
+}	else if (of.firstCount() > 0) {
 	of.openPerspective('First Thing');
-} else if (of.flaggedCount > 0) {
+} else if (of.flaggedCount() > 0) {
 	of.openPerspective("Flagged");
 } else {
 	of.openPerspective("Upcoming");
