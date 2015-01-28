17e11fe44da9213ccfca56bc4d9bf78026eb86d0
Brandon Pittman <brandonpittman@gmail.com>
2015-01-28 10:48:55 +0900

    Update smartPerspective.js
    Fix indenting

--- smartPerspective.js
+++ smartPerspective.js
@@ -1,7 +1,7 @@
 var of = Library('OmniFocus');
 
 if (of.inboxCount() > 0) {
-	of.openPerspective("Inbox");
+  of.openPerspective("Inbox");
 }	else if (of.firstCount() > 0) {
 	of.openPerspective('First Thing');
 } else if (of.flaggedCount() > 0) {
