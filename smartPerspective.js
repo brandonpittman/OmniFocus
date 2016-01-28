ff67fb7470f72f564b08fd960be91ad08be233af
brandonpittman <brandonpittman@gmail.com>
2016-01-28 11:05:58 +0900

    Add comment to smartPerspective and fix indentation

--- smartPerspective.js
+++ smartPerspective.js
@@ -1,11 +1,16 @@
-var of = Library('OmniFocus');
+// I've stopped using this script as of 2016-01-28. It served me well over the
+// last couple years, but I've decided that it's much better to consciously
+// decided what it is that you want to be looking at at any given moment—rather
+// than letting the computer decide what MODE you should be in.
+
+var of = Library("OmniFocus");
 
 if (of.inboxCount() > 0) {
-  of.openPerspective("Inbox");
+  of.openPerspective("Inbox")
 }	else if (of.firstCount() > 0) {
-	of.openPerspective('First Thing');
+  of.openPerspective("First Thing")
 } else if (of.flaggedCount() > 0) {
-	of.openPerspective("Flagged");
+  of.openPerspective("Flagged")
 } else {
-	of.openPerspective("Upcoming");
+  of.openPerspective("Upcoming")
 }
